533d87a198e9f4c690fd8d267980d825e0fa1247
Daniel-T-Horga <tom-horga@hotmail.com>
2015-12-21 15:46:43 -0500

    Reworked Shooting, Camera Control and movement

--- 2d-tactical-team-shooter/Assets/Scripts/Character_Controller.cs
+++ 2d-tactical-team-shooter/Assets/Scripts/Character_Controller.cs
@@ -15,6 +15,8 @@ public class Character_Controller : NetworkBehaviour {
 
     public float SensitivityX; // Sensitivity of the X movement of the character
     public float SensitivityY; // Sensitivity of the Y movement of the character
+    public GameObject camera;
+    public GameObject cameraPrefab;
 
 	// Use this for initialization
 	void Start () {
@@ -26,22 +28,56 @@ public class Character_Controller : NetworkBehaviour {
         if (isLocalPlayer)
         {
             Movement();
+            CameraFollow();
         }
+
          
 	}
 
 
 	void Movement()
         /*
-        Class manages the movement of the player.
+        Class manages the movement of the player. Also calculates the rotation
+        of the player and syncs it in multiplayer by called CmdCharacterRotation.
+        Also calls CameraFollowClass
 
         @rtype: None
         */
 	{
         float x = Input.GetAxis("Horizontal") * SensitivityX;
         float y = Input.GetAxis("Vertical") * SensitivityY;
-        transform.Translate(x, y, 0);
-	}
+        transform.position = transform.position + new Vector3(x, y, 0);
+
+        Vector3 diff = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        diff.Normalize();
+        float rot_z = Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg;
+        CmdCharacterRotation(rot_z);
+
+        CameraFollow();
+         
+    }
+
+    [Command]
+    void CmdCharacterRotation(float rot_z)
+    {
+        /*
+        sets the player rotation accordingly and syncs it in multiplayer.
+
+
+        @type rot_z: float
+            The Z rotation to be set for the cahracter
+        */
+        transform.rotation = Quaternion.Euler(0f, 0f, rot_z);
+    }
+
+    void CameraFollow()
+        /*
+        Makes the Camera Follow the player
+        */
+    {
+        camera.transform.position = new Vector3(transform.position.x, transform.position.y, camera.transform.position.z);
+    }
+
 
 
     public override void OnStartLocalPlayer()
@@ -53,6 +89,8 @@ public class Character_Controller : NetworkBehaviour {
         */
     {
         GetComponent<SpriteRenderer>().color = Color.blue;
-        GetComponentInChildren<Camera>().enabled = true;
+        //GetComponentInChildren<Camera>().enabled = true;
+        Vector3 position = new Vector3(transform.position.x, transform.position.y, -10);
+        camera = (GameObject)Instantiate(cameraPrefab, position, transform.rotation);
     }
 }
